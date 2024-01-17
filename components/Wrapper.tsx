6fc598bd4d782984ea8bf6a5c74abeea09812857
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2024-01-17 19:37:35 +0300

    chore: remove all of contents

--- components/Wrapper.tsx
+++
@@ -1,8 +0,0 @@
-import { BaseComponent } from '@/types/BaseComponent';
-import { createElement } from 'react';
-
-export type WrapperProps = BaseComponent & {};
-
-export default function Wrapper({ as = 'div', children }: WrapperProps) {
-  return createElement(as, { className: 'min-h-screen flex p-6 pt-12 xl:px-16' }, children);
-}
--- types/BaseComponent.ts
+++
@@ -1,6 +0,0 @@
-import { ElementType, ReactNode } from "react"
-
-export type BaseComponent = {
-  children?: ReactNode[],
-  as? : ElementType,
-}
